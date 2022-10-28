66da3d9d1952bd6c8f43a484100bda88849baabf
Thyago <thyagoalmeidaprof@gmail.com>
2022-10-27 22:26:46 -0300

    V_005

--- projeto/tuxview/src/app/inicio/inicio.component.ts
+++ projeto/tuxview/src/app/inicio/inicio.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  imagemHome = "https://cdn.pixabay.com/photo/2014/08/23/19/50/photography-425687_960_720.jpg";
+  /* imagemHome = "https://cdn.pixabay.com/photo/2014/08/23/19/50/photography-425687_960_720.jpg"; */
+  imagemHome = "https://cdn.pixabay.com/photo/2013/05/02/06/08/camera-108531_960_720.jpg";
   constructor() { }
 
   ngOnInit(): void {
