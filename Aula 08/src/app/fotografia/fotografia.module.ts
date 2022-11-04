f27219cef64a089aa44b4f6cc373052956b45581
Thyago <thyagoalmeidaprof@gmail.com>
2022-11-04 20:57:58 -0300

    V_008

---
+++ Aula 08/src/app/fotografia/fotografia.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+
+import { FotoComponent } from "./foto/foto.component";
+import { FormularioComponent } from "./formulario/formulario.component";
+import { InicioComponent } from "./inicio/inicio.component";
+import { SharedModule } from "../shared/shared.module";
+
+
+@NgModule({
+    //Determina quais componentes são de responsabilidade do modulo
+    declarations: [
+        FotoComponent,
+        FormularioComponent,
+        InicioComponent
+    ],
+
+    
+    imports: [
+        BrowserModule,        
+    ],
+
+    //Permite que outros modulos ou componentes acessem este grupo
+    exports: [
+        FotoComponent,
+        FormularioComponent
+    ]
+   
+})
+export class FotografiaModule{}
